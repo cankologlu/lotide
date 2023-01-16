fe86eff408762df2b2159d34cb467ffbee8381ac
Can Kologlu <mckologlu@gmail.com>
2023-01-16 16:08:14 -0500

    ocha and chai to run test and conversion of headTest.js to the format

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,23 @@
-const assertEqual = require("../assertEqual.js");
+//const assertEqual = require("../assertEqual.js");
 const head = require("../head.js");
 
-console.log("head() returns the first element of the passed array")
-assertEqual(head(["",6,7]), "");
-assertEqual(head(["abc",5,6,7]), "abc");
-assertEqual(head([5,6,7]), 5);
-assertEqual(head([" "]), " ");
-assertEqual(head([]),);
+const assert = require("chai").assert;
+
+describe("#head", () => {
+  it("returns 1 for [1,2,3]", () => {
+    assert.strictEqual(head([1,2,3]),1);
+  });
+  it("returns '5' for [5]", () => {
+    assert.strictEqual(head([5]), 5);
+  });
+  it("returns '' for ['',6,7] ", () => {
+    assert.strictEqual(head(["",6,7]), "");
+  });
+  it("returns abc for ['abc',5,6,7]", () => {
+    assert.strictEqual(head(["abc",5,6,7]), "abc")
+  });
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]),undefined);
+  });
+});
+
